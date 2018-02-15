c6f404c6514c712917754b207d32278a733dee83
chessin2000 <cassinl202@gmail.com>
2018-02-15 11:37:21 +0100

    php start

--- ASLDBCRUD.php
+++ ASLDBCRUD.php
@@ -4,3 +4,8 @@ $first_name = $_GET['nome'];
 $last_name = $_GET['cognome'];
 $email = $_GET['mail'];
 
+echo ("<input type=\"text\" name=\"nome\" placeholder=\"Inserisci il nome\">
+<input type=\"text\" name=\"cognome\" placeholder=\"Inserisci il cognome\">
+<input type=\"email\" name=\"mail\" placeholder=\"Inserisci l'email\">
+<input type=\"button\" name=\"modifica\" value=\"modifica\">
+<input type=\"button\" name=\"cancella\" value=\"cancella\">");
