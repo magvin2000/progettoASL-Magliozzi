c3f6016a7a3ebd758ae7d252217bbb0d403caa08
magvin2000 <magvin805gmail.com>
2018-02-15 14:43:49 +0100

    direi che è meglio.

--- ASLDBCRUD.php
+++ ASLDBCRUD.php
@@ -1,11 +1,12 @@
+
+
 <?php
 
-echo ("<input type=\"text\" name=\"nome\" placeholder=\"Inserisci il nome\">
-<input type=\"text\" name=\"cognome\" placeholder=\"Inserisci il cognome\">
-<input type=\"email\" name=\"mail\" placeholder=\"Inserisci l'email\">
-<input type=\"button\" name=\"modifica\" value=\"Modifica\">
-<input type=\"button\" name=\"cancella\" value=\"Cancella\">");
 
-$first_name = $_GET['nome'];
-$last_name = $_GET['cognome'];
-$email = $_GET['mail'];
+$servername = "localhost";
+$username = "root";
+$password="";
+
+$conn = mysqli_connect($servername, $username, $password);
+$sql = "SELECT id, nome, cognome FROM DBASL";
+$result = $conn->query($sql);
