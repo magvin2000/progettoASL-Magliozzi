4286e495931415688cb22da705b4ed0cd50139b2
chessin2000 <cassinl202@gmail.com>
2018-02-16 09:42:08 +0100

    delete++

--- button_elimina.php
+++ button_elimina.php
@@ -1,1 +1,27 @@
 <?php
+
+$Nome=$_GET['nome'];
+$Cognome=$_GET['cognome'];
+$Email=$_GET['email'];
+$servername = "localhost";
+$username = "root";
+$password="";
+$dbname="nuovo";
+$ID=1;
+// Create connection
+$conn = new mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "DELETE FROM dbasl WHERE some_column = some_value";
+
+    if ($conn->query($sql) === TRUE) {
+    echo "Record deleted successfully";
+} else {
+    echo "Error deleting record: " . $conn->error;
+}
+
+$conn->close();
+?>
