0bf23765754d2122dc7f415cd4ab1d5f72af1a62
magvin2000 <magvin805gmail.com>
2018-02-19 12:05:27 +0100

    versione migliore

--- button_aggiorna.php
+++ button_aggiorna.php
@@ -8,38 +8,39 @@
     <title>DATABASE</title>
 </head>
 <body>
-        <h2>Modifica i dati</h2>
+
+<form action='' method='get'>
+    <h2>Modifica i dati</h2>
+    <input type='text' name='nome' placeholder='Nome' value="<?php echo $_GET['nome']; ?>" required>
+    <input type='text' name='cognome' placeholder='Cognome' value='<?php echo $_GET['cognome']; ?>' required>
+    <input type='email' name='email' placeholder='Email' value='<?php echo $_GET['email']; ?>' required>
+    <input type ='hidden' name ='Id' value ='<?php echo $_GET['Id']; ?>'>
+    <br>
+    <input type='submit' value='Aggiorna' name='Aggiorna' class='update_button'>
+</form>
+
 <?php
+//if (isset($_GET['nome']) && isset($_GET['cognome']) && isset($_GET['email'])) {
+    $Nome = $_GET['nome'];
+    $Cognome = $_GET['cognome'];
+    $Email = $_GET['email'];
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "nuovo";
+    $id = $_GET['Id'];
 
-$Nome=$_GET['nome'];
-$Cognome=$_GET['cognome'];
-$Email=$_GET['email'];
-$servername = "localhost";
-$username = "root";
-$password="";
-$dbname="nuovo";
-$id = $_GET['Id'];
+    $conn = new mysqli($servername, $username, $password, $dbname);
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+    if (!$conn) {
+        die("Connessione non stabilita: " . mysqli_connect_error());
+    }
 
-if (!$conn) {
-    die("Connessione non stabilita: " . mysqli_connect_error());
-}
+    $sql = "UPDATE dbasl SET NOME='$Nome', COGNOME='$Cognome', EMAIL='$Email' WHERE ID='$id'";
 
-echo ("<form action='' method='get'>");
-echo ("<input type='text' name='nome' placeholder='Nome' value='$Nome' required>");
-echo ("<input type='text' name='cognome' placeholder='Cognome' value='$Cognome' required>");
-echo ("<input type='email' name='email' placeholder='Email' value='$Email' required>");
-echo ("<input type ='hidden' name ='Id' value ='$id'>");
-echo ('<br>');
+    $conn->query($sql);
+    //header("location:http://localhost/progettoASL-MagliozziCassin/ASLDBCRUD.php");
+    $conn->close();
 
-$sql = "UPDATE dbasl SET NOME='$Nome', COGNOME='$Cognome', EMAIL='$Email' WHERE ID='$id'";
-
-$conn->query($sql);
-echo ("<input type='submit' value='Aggiorna' name='Aggiorna' class='update_button'>");
-echo ("</form>");
-
-
-$conn->close();
 ?>
 </body>
