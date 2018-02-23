c19aba7aef8edb8a5150b1c2a249e29c694809bc
chessin2000 <cassinl202@gmail.com>
2018-02-23 10:17:29 +0100

    nuove funzioni...

--- javascript_funzioni.js
+++ javascript_funzioni.js
@@ -11,7 +11,6 @@ function ricerca()
         }
         else
         {
-
             if (casella[i].innerHTML.toUpperCase().search(ricerca.toUpperCase()) > -1 && (casella[i].innerHTML.search("<")) === -1)
             {
                 casella[i].style.backgroundColor = "#000000";
@@ -26,7 +25,100 @@ function ricerca()
     }
 }
 
-function select()
-{
-    
+function ordinamento(tabella,elementi){
+    var appoggio=new Array();
+    var appoggioTabella=new Array();
+    for(var i=0;i<elementi.length;i++)
+    {
+        appoggio[i]=elementi[i].innerHTML + i.toString();
+        appoggioTabella[i]=tabella[i+1].innerHTML;;
+    }
+    appoggio.sort();
+
+    for(var i=0;i<appoggio.length;i++)  tabella[i+1].innerHTML=appoggioTabella[appoggio[i][appoggio[i].length-1]];
+}
+
+function selezione(Tabella) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            Tabella.innerHTML =   "<thead> " +
+                "<th> Id      </th>" +
+                "<th> Nome    </th>" +
+                "<th> Cognome </th>" +
+                "<th> Email   </th>" +
+                "<th> Update  </th>" +
+                "<th> Delete  </th>" +
+                "</thead>" +
+                this.responseText;
+        }
+    };
+    xhttp.open("GET", "Select.php", true);
+    xhttp.send();
+}
+
+function formazioneformboh(elemento,tipo,id){
+    var nome="",cognome="",email="";
+    appoggio=elemento.innerHTML;
+    if(tipo=="Update"){
+        nome=elemento.getElementsByTagName('input')[0].value;
+        cognome=elemento.getElementsByTagName('input')[1].value;
+        email=elemento.getElementsByTagName('input')[2].value;
+    }
+    elemento.innerHTML = "Nome<input type=\"text\" name=\"nome\" value='" + nome + "'    required>\n" +
+        "Cognome<input type=\"text\" name=\"cognome\"  value='" + cognome + "'  required>\n" +
+        "Mail<input type=\"email\" name=\"mail\" value=" + email + ">\n" +
+        "<input type='hidden' name='Identificativo' value=" + id + ">\n";
+    if(tipo=="Aggiungi")elemento.innerHTML+="<button class=\"btn btn-success glyphicon glyphicon-envelope\" onclick=\"Aggiungi(this.parentNode)\"> Invia</button>\n";
+    else if(tipo=="Update")elemento.innerHTML+="<button class=\"btn btn-success glyphicon glyphicon-envelope\" onclick=\"Update(this.parentNode)\"> Invia</button>\n";
+    elemento.innerHTML+="<button id='Annulla' class='btn btn-danger glyphicon glyphicon-remove' onclick='Annulla(this.parentNode)'> Annulla</button>";
+}
+
+function aggiunta(elemento) {
+    var xhttp = new XMLHttpRequest();
+    var nome=elemento.getElementsByTagName('input')[0].value;
+    var cognome=elemento.getElementsByTagName('input')[1].value;
+    var email=elemento.getElementsByTagName('input')[2].value;
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            if(this.responseText!="")  elemento.getElementsByTagName('input')[0].value=this.responseText;
+            else {
+                elemento.innerHTML = "<button class=\"btn btn-success glyphicon glyphicon-plus\" onclick=\"Form(document.getElementById('p'),'Aggiungi',0);\"> Aggiungi</button>";
+                Select(document.getElementById('Tabella'));
+            }
+        }
+    };
+    xhttp.open("GET", "Aggiungi.php?nome=" + nome + "&cognome=" + cognome + "&email=" + email, true);
+    xhttp.send();
+}
+
+function aggiornamento(elemento) {
+    var xhttp = new XMLHttpRequest();
+    var nome=elemento.getElementsByTagName('input')[0].value;
+    var cognome=elemento.getElementsByTagName('input')[1].value;
+    var email=elemento.getElementsByTagName('input')[2].value;
+    var id=elemento.getElementsByTagName('input')[3].value;
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            if(this.responseText!="")  elemento.getElementsByTagName('input')[0].value=this.responseText;
+            else Select(document.getElementById('Tabella'));
+        }
+    };
+    xhttp.open("GET", "Update.php?nome=" + nome + "&cognome=" + cognome + "&email=" + email +"&Identificativo="+id, true);
+    xhttp.send();
+}
+
+function eliminazione(Id){
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            Select(document.getElementById('Tabella'));
+        }
+    }
+    xhttp.open("GET", "Delete.php?Identificativo="+Id, true);
+    xhttp.send();
+}
+
+function annullamento(elemento) {
+    elemento.innerHTML=appoggio;
 }
